430fe3eb80c1e903ff2be60fdd93a817ee8befd0
signorazai <zairajuliaa@gmail.com>
2024-08-06 11:10:24 +0800

    Layout + Design Updated

--- resources/views/admin/messages.blade.php
+++ resources/views/admin/messages.blade.php
@@ -4,13 +4,11 @@
 
 @section('content')
 <div class="bg-white p-6 rounded-lg shadow-md">
-    <h1 class="text-3xl font-bold mb-6">Broadcast Messages</h1>
-
     <form action="{{ route('admin.send-messages') }}" method="POST">
         @csrf
 
         <!-- Flexbox container for dropdowns -->
-        <div class="flex space-x-4 mb-4">
+        <div class="flex space-x-4 mb-8">
             <div class="w-1/4">
                 <label for="campus" class="block text-sm font-medium text-gray-700">Campus</label>
                 <select name="campus" id="campus" class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-indigo-300 p-2">
@@ -56,11 +54,13 @@
         <!-- Input Message -->
         <div class="mb-4">
             <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
-            <textarea name="message" id="message" rows="4" class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-indigo-300 p-2"></textarea>
+            <textarea name="message" id="message" placeholder="Enter your message here ..." rows="4" class="block w-full mt-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-indigo-300 p-2 text-sm overflow-y-auto resize-none" style="height: 14rem"></textarea>
         </div>
 
-
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Send</button>
+        <!-- Send Button Here -->
+        <div class="flex justify-end">
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Send</button>
+        </div>
     </form>
 </div>
 @endsection
