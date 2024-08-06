dbd8b22a352c61c4b0a9975a87960358164a174f
joshdrey33 <joshuaandreybanuelos@gmail.com>
2024-08-06 14:23:06 +0800

    H1 Removed - Redundant

--- resources/views/admin/analytics.blade.php
+++ resources/views/admin/analytics.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
 <div class="bg-white p-6 rounded-lg shadow-md">
-    <h1 class="text-3xl font-bold mb-6">Analytics</h1>
     <p>Here you can view analytics data.</p>
 </div>
 @endsection
