7cc5f87a6558da257ad821bb661194db1711b727
signorazai <124949934+signorazai@users.noreply.github.com>
2024-10-14 10:58:52 +0800

    Integrated the Button Components in Review Message Subadmin

--- resources/views/subadmin/review-message.blade.php
+++ resources/views/subadmin/review-message.blade.php
@@ -91,10 +91,14 @@
             @endif
 
             <!-- Edit Message Button -->
-            <a href="{{ route('subadmin.messages', $data) }}" class="bg-yellow-500 text-white px-4 py-2 rounded-lg mr-2 inline-block text-center">
-                Edit Message
-            </a> 
-            <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-lg">Confirm and Send</button>
+            <x-button type="button" color="gray">
+                <a href="{{ route('subadmin.messages', $data) }}">
+                    Edit Message
+                </a>
+            </x-button>
+            <x-button type="submit" color="red">
+                Confirm and Send
+            </x-button>
         </form>
     </div>
 
